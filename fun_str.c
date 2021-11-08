05e5f52206df30730fd6c621eed841312065f3af
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-08 13:57:50 -0800

    test

--- fun_str.c
+++ fun_str.c
@@ -12,6 +12,7 @@ int fun_str(va_list ap)
 	int aux;
 	int counter = 0;
 
+	if (str)
 	for (aux = 0; str[aux] != 0; aux++)
 	{
 		write(1, str + aux, 1);
--- test/1-main.c
+++ test/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("\n");
 	_printf("%s tiene %u años de edad, su primo tiene %u tmb", name, age, 99);
 	printf("\n");
-	counter = _printf("probando el %% doble");
+	counter = printf("es %d", -12);
 	printf("\n%d", counter);
 	return (0);
 }
