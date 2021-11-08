ece54ddadb6c8a2471b34e0f9609bc6bdc13666c
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-08 10:19:36 -0800

    agregado prototype en header

--- test/1-main.c
+++ test/1-main.c
@@ -2,10 +2,10 @@
 int main(void)
 {
 	char *name = "Juan Carlos";
-	char censura = 64;
+	char censura = '*';
 	unsigned int age = 10;
 
-	_printf("hola %s como estas chupa pij%c", name, censura);
+	_printf("hola %s como estas chu%c%c pij%c", name, censura, censura, censura);
 	printf("\n");
 	_printf("%s tiene %u años de edad, su primo tiene %u tmb", name, age, 99);
 	printf("\n");
