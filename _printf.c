7902e63f7996f6bcaceb62b0dd38003c20c97aaf
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-11-08 17:04:38 -0800

    If de NULL

--- _printf.c
+++ _printf.c
@@ -23,6 +23,10 @@ int _printf(const char *format, ...)
 	va_start(ap, format);
 	for (i = 0; format && format[i] != 0; i++)
 	{
+		if (!format)
+		{
+			return (-1);
+		}
 		if (format[i] == '%')
 		{
 			i++;
